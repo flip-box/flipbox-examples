d574a902b245306169315186711535c8d0fb8a99
Arete Code <aretecode@gmail.com>
2017-03-06 08:12:17 -0800

    📚 add examples

---
+++ flip.js
@@ -0,0 +1,33 @@
+// @TODO: use real commander
+// var path = require('path')
+global._dirname = __dirname
+
+// const {cli} = require('../../src')
+// console.log(cli)
+require('./configs/z/entry.js')
+// cli.flipHandler(require('./configs/z/entry.js'))
+
+// require('./configs/minimal')
+
+// global.compile = true
+// global.exec = true
+// global.run = true
+// console.log(__dirname)
+// process.env.argv.push('bin/webpack')
+// process.env.NODE_ENV = 'production'
+
+// @NOTE: should have the commander be doing this dynamically
+// var output = require('./configs/basic/mediator')
+
+// require('./configs/basic/mediator')
+// require('./configs/basic-build/mediator')
+// require('./configs/intermediate/mediator')
+
+// require('./configs/verbose/mediator')
+// require('./configs/node/mediator')
+
+// require('./configs/intermediate-tests/mediator')
+// require('./configs/fusebox/mediator')
+
+// require('./configs/fuse-canadas/mediator')
+// require('./configs/compat/mediator')
