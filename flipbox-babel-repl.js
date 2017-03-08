488b7f5b09863f99bc9161ff9b631763d95974c3
Arete Code <aretecode@gmail.com>
2017-03-07 17:52:44 -0800

    babel playground fun

--- flipbox-babel-repl.js
+++ flipbox-babel-repl.js
@@ -14,9 +14,22 @@ const config = {
     alias: ['moose', 'igloo'],
     presets: ['node'],
     entry: './src/back/index.js',
+
+    // babel fun
+    loaders: {
+      babel: {
+        config: {
+          // presets: ['env'],
+          presets: [['env', {
+            'targets': {
+              'chrome': 56,
+            },
+            'debug': true,
+          }]],
+        },
+      },
+    },
   }],
 }
 const FlipBox = require('./flipbox')
 const builder = FlipBox.init(config).fullAuto()
-
-module.exports = builder.mediator()
